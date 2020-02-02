dc2159ac13dce31511dc249e21deb15be7a3ce4d
proelbtn <contact@proelbtn.com>
2020-02-02 10:11:52 +0000

    update

--- scripts/setup-stage2.sh
+++ scripts/setup-stage2.sh
@@ -11,8 +11,9 @@ fi
 machinectl list-images | grep -q m1 || sudo machinectl clone ${BASEIMAGE} m1
 machinectl list-images | grep -q m2 || sudo machinectl clone ${BASEIMAGE} m2
 
+sudo systemd-nspawn -D /var/lib/machines/m1 bash -c "useradd -s /bin/bash -m -G sudo -p '$6$feoWBZt29xWXIJhO$oHQCbGSFGnUgzrVw9gaXk0lbAuF7bHAfI8spR.a8H3IXF./6rq3quIoezF7Fs9EWMkna4qTGPN08u5O0ss6V20' user"
 sudo systemd-nspawn -D /var/lib/machines/m1 bash -c "rm -rf /etc/resolv.conf && echo 'nameserver 8.8.8.8' > /etc/resolv.conf"
-sudo systemd-nspawn -D /var/lib/machines/m1 bash -c "apt update && apt install -y nginx openssh-server"
+sudo systemd-nspawn -D /var/lib/machines/m1 bash -c "apt update && apt install -y ethtool nginx openssh-server tcpdump"
 sudo systemd-nspawn -D /var/lib/machines/m1 bash -c "cat <<EOF | tee /etc/netplan/00_host0.yaml && netplan generate
 network:
   version: 2
@@ -28,8 +29,9 @@ network:
           - 8.8.4.4
 EOF"
 
+sudo systemd-nspawn -D /var/lib/machines/m2 bash -c "useradd -s /bin/bash -m -G sudo -p '$6$feoWBZt29xWXIJhO$oHQCbGSFGnUgzrVw9gaXk0lbAuF7bHAfI8spR.a8H3IXF./6rq3quIoezF7Fs9EWMkna4qTGPN08u5O0ss6V20' user"
 sudo systemd-nspawn -D /var/lib/machines/m2 bash -c "rm -rf /etc/resolv.conf && echo 'nameserver 8.8.8.8' > /etc/resolv.conf"
-sudo systemd-nspawn -D /var/lib/machines/m2 bash -c "apt update && apt install -y apache2 openssh-server"
+sudo systemd-nspawn -D /var/lib/machines/m2 bash -c "apt update && apt install -y apache2 ethtool openssh-server tcpdump"
 sudo systemd-nspawn -D /var/lib/machines/m2 bash -c "cat <<EOF | tee /etc/netplan/00_host0.yaml && netplan generate
 network:
   version: 2
@@ -49,6 +51,7 @@ sudo machinectl enable m1
 sudo machinectl enable m2
 
 sudo systemctl daemon-reload
+sudo systemctl enable setup-veth
 
 cd /opt/forwarder
 make build
